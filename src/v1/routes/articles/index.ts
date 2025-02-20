4ec3aa3aae408e3ff35272e467f84ee4c5d985a9
Prabin Subedi <42871240+prabincankod@users.noreply.github.com>
2025-02-20 22:35:32 +0545

    minor changes in business logic

--- src/v1/routes/articles/index.ts
+++ src/v1/routes/articles/index.ts
@@ -4,31 +4,38 @@ import { prismaClient } from "../../../../prisma/prisma";
 
 export const articlesRouter = Router();
 
-articlesRouter.get("/", async (req: Request, res: Response) => {
-  res.send("artiles lies here");
-});
 articlesRouter.post("/", async (req: Request, res: Response) => {
   const body = req.body;
   const parsed = await CreateArticle.spa(body);
 
+  if (!parsed.success) {
+    res.status(401).json({ success: false, message: "check the request body" });
+  }
+
   try {
     if (parsed.success && parsed.data) {
-      const created = await prismaClient.article.create({ data: parsed.data });
-
+      await prismaClient.article.create({ data: parsed.data });
       res
         .status(201)
         .json({ success: true, message: "article creation success" });
-    } else {
-      console.log(parsed.error);
-      res
-        .status(401)
-        .json({ success: false, message: "check the request body" });
     }
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, message: "something went wrong" });
   }
 });
-articlesRouter.get("/", async (req: Request, res: Response) => {
-  res.send("artiles lies here");
+
+articlesRouter.get("/:slug", async (req: Request, res: Response) => {
+  const slug = req.params.slug;
+  const article = await prismaClient.article.findUnique({
+    where: {
+      slug: slug,
+    },
+  });
+
+  if (!article) {
+    res.status(404).json({ success: false, message: "not found" });
+  }
+
+  res.status(200).json({ success: true, data: article });
 });
