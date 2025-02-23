69636459cd99a316f599432520e92950e4855719
Prabin Subedi <42871240+prabincankod@users.noreply.github.com>
2025-02-23 20:57:27 +0545

    ran prettier

--- src/v1/routes/categories/index.ts
+++ src/v1/routes/categories/index.ts
@@ -1,51 +1,41 @@
 import { Router } from "express";
 import { prismaClient } from "../../../../prisma/prisma";
 
-export const categoriesRouter = Router()
+export const categoriesRouter = Router();
 
-categoriesRouter.get('/:slug', async (req, res) => {
+categoriesRouter.get("/:slug", async (req, res) => {
+  const slug = req.params.slug;
 
-    const slug = req.params.slug;
+  // const posts = await prismaClient.article.findMany({
+  //     where: {
+  //         categories: {
+  //             some: {
+  //                 slug: slug
+  //             }
+  //         }
+  //     }
+  // })
 
-    // const posts = await prismaClient.article.findMany({
-    //     where: {
-    //         categories: {
-    //             some: {
-    //                 slug: slug
-    //             }
-    //         }
-    //     }
-    // })
+  const category = prismaClient.category.findUnique({
+    where: {
+      slug: slug,
+    },
+    include: {
+      articles: {
+        omit: {
+          content: true,
+        },
+        include: {
+          ogImage: true,
+        },
+      },
+      image: true,
+    },
+  });
 
-    const category = prismaClient.category.findUnique({
-        where: {
-            slug: slug
-        }, include: {
-            articles: {
-                omit: {
-                    content: true
-                },
-                include: {
-                    ogImage: true
-                }
-            },
-            image: true
-        }
-    })
+  if (!category) {
+    res.status(404).json({ success: false, message: "category not found" });
+  }
 
-
-    if (!category) {
-        res.status(404).json({ success: false, message: 'category not found' })
-    }
-
-
-    res.json({ success: true, data: category })
-
-
-
-
-
-
-
-
-})
+  res.json({ success: true, data: category });
+});
